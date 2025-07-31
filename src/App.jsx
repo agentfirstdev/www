4a5c5f81746ebc8d92978ff4bacc3db3a4f53198
Brian Kennish <oldestlivingboy@gmail.com>
2025-07-31 13:07:14 -0500

    Copyedited again.

--- src/App.jsx
+++ src/App.jsx
@@ -392,8 +392,8 @@ export default function App() {
                 <Text as='span' textDecoration='line-through'>
                   dog
                 </Text>
-                llamafooding that is, a “meta-LLM” that combines responses from popular large
-                {' language models in one interface, called '}
+                llamafooding that is, a “meta-LLM” that evaluates and merges responses from popular
+                {' large language models, called '}
                 <Link variant='team' href={ui.demoUrl} isExternal>
                   Llamapile
                 </Link>
