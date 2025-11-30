580267c2ff50e4604d60ee16f52e986174c420c4
Brian Kennish <oldestlivingboy@gmail.com>
2025-11-30 14:32:04 -0600

    Copyedited yet even more.

--- src/App.jsx
+++ src/App.jsx
@@ -935,8 +935,8 @@ export default function App() {
           />
         </Box>
         <Text variant='about' mx={ui.lgMargin} mt={ui.mdMargin} textAlign='left'>
-          Many of the most successful technology companies started when developers figured out how
-          to build natively on emerging platforms, from desktop computing to AI.
+          Many of the most successful technology companies started with developers who figured out
+          how to build natively on emerging platforms, from desktop computing to AI.
         </Text>
         <Text variant='about' mx={ui.lgMargin} mt={ui.smMargin} textAlign='left'>
           We’re betting agent-first development is the next big opportunity and are providing
