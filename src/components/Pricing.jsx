119fbf09d89e52a3bf932ecf6489319c641d949e
Brian Kennish <oldestlivingboy@gmail.com>
2026-02-07 01:35:58 -0800

    Separated calculator.

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,64 @@
+import {
+  Flex,
+  InputGroup,
+  InputLeftElement,
+  NumberInput,
+  NumberInputField,
+  Button
+} from '@chakra-ui/react';
+
+import * as ui from '../config/ui';
+
+const creditsPerDollar = 1000 / ui.cpm;
+const discountedCreditsPerDollar = 1000 / ui.discountedCpm;
+const calculateCredits = (dollars) => {
+  return Math.ceil(
+    Math.floor(dollars) *
+      (dollars < ui.discountedPurchaseThreshold ? creditsPerDollar : discountedCreditsPerDollar)
+  ).toLocaleString();
+};
+
+export default function Pricing({ dollarAmount, setDollarAmount, isCartLoading, addToCart }) {
+  const parsedAmount = parseFloat(dollarAmount);
+  const isAmountValid = Number.isInteger(parsedAmount) && parsedAmount >= ui.minPurchaseAmount;
+
+  return (
+    <Flex mt={2} w={ui.secondaryTextboxWidth} direction='column' gap={4}>
+      <InputGroup>
+        <InputLeftElement
+          h={ui.controlDimension}
+          fontSize='lg'
+          color={dollarAmount ? 'chakra-body-text' : 'chakra-label-color'}
+          pointerEvents='none'
+        >
+          $
+        </InputLeftElement>
+        <NumberInput
+          w='100%'
+          precision={ui.purchaseDecimalPlaces}
+          min={ui.minPurchaseAmount}
+          value={dollarAmount}
+          onChange={setDollarAmount}
+          isValidCharacter={(character) => {
+            return character >= '0' && character <= '9';
+          }}
+        >
+          <NumberInputField
+            pl={ui.purchaseAmountPadding}
+            h={ui.controlDimension}
+            fontSize='lg'
+            placeholder={`${ui.minPurchaseAmount} minimum`}
+          />
+        </NumberInput>
+      </InputGroup>
+      <Button
+        h={ui.controlDimension}
+        isDisabled={!isAmountValid}
+        isLoading={isCartLoading}
+        onClick={addToCart}
+      >
+        {`Add${isAmountValid ? ` ${calculateCredits(parsedAmount)}` : ''} credits`}
+      </Button>
+    </Flex>
+  );
+}
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,32 +1,14 @@
 import { useState } from 'react';
-import {
-  Box,
-  Flex,
-  Heading,
-  InputGroup,
-  InputLeftElement,
-  NumberInput,
-  NumberInputField,
-  Button,
-  Spinner,
-  useToast
-} from '@chakra-ui/react';
+import { Box, Flex, Heading, Spinner, useToast } from '@chakra-ui/react';
 import { loadStripe } from '@stripe/stripe-js';
 import { EmbeddedCheckoutProvider, EmbeddedCheckout } from '@stripe/react-stripe-js';
 
 import * as ui from '../config/ui';
+import Pricing from '../components/Pricing';
 import Login from '../components/Login';
 
 const toastId = 'checkout';
-const creditsPerDollar = 1000 / ui.cpm;
-const discountedCreditsPerDollar = 1000 / ui.discountedCpm;
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
-const calculateCredits = (dollars) => {
-  return Math.ceil(
-    Math.floor(dollars) *
-      (dollars < ui.discountedPurchaseThreshold ? creditsPerDollar : discountedCreditsPerDollar)
-  ).toLocaleString();
-};
 
 export default function Checkout({ supabaseClient, session, isSessionLoading }) {
   const [amount, setAmount] = useState('');
@@ -34,7 +16,6 @@ export default function Checkout({ supabaseClient, session, isSessionLoading }) 
   const [isLoading, setIsLoading] = useState(false);
   const toast = useToast();
   const parsedAmount = parseFloat(amount);
-  const isAmountValid = Number.isInteger(parsedAmount) && parsedAmount >= ui.minPurchaseAmount;
   const addToCart = () => {
     setIsLoading(true);
 
@@ -77,43 +58,12 @@ export default function Checkout({ supabaseClient, session, isSessionLoading }) 
         </Flex>
       ) : (
         <Flex justify='center' align='start' flex={1}>
-          <Flex mt={2} w={ui.secondaryTextboxWidth} direction='column' gap={4}>
-            <InputGroup>
-              <InputLeftElement
-                h={ui.controlDimension}
-                fontSize='lg'
-                color={amount ? 'chakra-body-text' : 'chakra-label-color'}
-                pointerEvents='none'
-              >
-                $
-              </InputLeftElement>
-              <NumberInput
-                w='100%'
-                precision={ui.purchaseDecimalPlaces}
-                min={ui.minPurchaseAmount}
-                value={amount}
-                onChange={setAmount}
-                isValidCharacter={(character) => {
-                  return character >= '0' && character <= '9';
-                }}
-              >
-                <NumberInputField
-                  pl={ui.purchaseAmountPadding}
-                  h={ui.controlDimension}
-                  fontSize='lg'
-                  placeholder={`${ui.minPurchaseAmount} minimum`}
-                />
-              </NumberInput>
-            </InputGroup>
-            <Button
-              h={ui.controlDimension}
-              isDisabled={!isAmountValid}
-              isLoading={isLoading}
-              onClick={addToCart}
-            >
-              {`Add${isAmountValid ? ` ${calculateCredits(parsedAmount)}` : ''} credits`}
-            </Button>
-          </Flex>
+          <Pricing
+            dollarAmount={amount}
+            setDollarAmount={setAmount}
+            isCartLoading={isLoading}
+            addToCart={addToCart}
+          />
         </Flex>
       )}
     </>
